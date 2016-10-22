a1e0de52c21fdeaa219901ec6508ee0c6fea2e87
Kamil <k.mucik@e-strix.pl>
2016-10-22 10:30:30 +0200

    prepare maven project

---
+++ src/main/java/pl/estrix/AppConfiguration.java
@@ -0,0 +1,12 @@
+package pl.estrix;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ComponentScan(basePackages="com.benjsicam")
+public class AppConfiguration {
+	
+}
